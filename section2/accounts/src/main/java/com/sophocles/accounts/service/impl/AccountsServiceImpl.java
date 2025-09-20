bfed4fed1b0eaefc7792fa9bf236671fbd414bcd
arjun292619 <j.c.arjunreddy@gmail.com>
2025-09-20 08:51:02 -0700

    Add create account path with exceptions

--- section2/accounts/src/main/java/com/sophocles/accounts/service/impl/AccountsServiceImpl.java
+++ section2/accounts/src/main/java/com/sophocles/accounts/service/impl/AccountsServiceImpl.java
@@ -1,11 +1,20 @@
 package com.sophocles.accounts.service.impl;
 
+import com.sophocles.accounts.constants.AccountsConstants;
 import com.sophocles.accounts.dto.CustomerDto;
+import com.sophocles.accounts.entity.Accounts;
+import com.sophocles.accounts.entity.Customer;
+import com.sophocles.accounts.exceptions.CustomerAlreadyExistsException;
+import com.sophocles.accounts.mapper.CustomerMapper;
 import com.sophocles.accounts.repository.AccountsRepository;
 import com.sophocles.accounts.repository.CustomerRepository;
 import com.sophocles.accounts.service.IAccountsService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.Random;
 
 @Service
 @AllArgsConstructor
@@ -16,6 +25,29 @@ public class AccountsServiceImpl implements IAccountsService {
 
     @Override
     public void createAccount(CustomerDto customerDto) {
+        Customer customer = CustomerMapper.mapToCustomer(customerDto, new Customer());
+        Optional<Customer> optionalCustomer = customerRepository.findByMobileNumber(customerDto.getMobileNumber());
+        if (optionalCustomer.isPresent()) {
+            throw new CustomerAlreadyExistsException("Customer is already registered with given Mobile Number"
+                    + customerDto.getMobileNumber());
+        }
+        customer.setCreatedAt(LocalDateTime.now());
+        customer.setCreatedBy("Admin");
+        Customer savedCustomer = customerRepository.save(customer);
+        accountsRepository.save(createNewAccount(savedCustomer));
+    }
 
+    private Accounts createNewAccount(Customer customer) {
+        Accounts accounts = new Accounts();
+        accounts.setCustomerId(customer.getCustomerId());
+        long randomAccNumber = 1000000000L + new Random().nextInt(900000000);
+
+        accounts.setAccountNumber(randomAccNumber);
+        accounts.setAccountType(AccountsConstants.SAVINGS);
+        accounts.setBranchAddress(AccountsConstants.ADDRESS);
+        accounts.setCreatedBy("Admin");
+        accounts.setCreatedAt(LocalDateTime.now());
+        return accounts;
     }
 }
+
