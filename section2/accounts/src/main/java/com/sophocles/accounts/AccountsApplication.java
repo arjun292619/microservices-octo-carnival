c3d9bb0e9b0947bf751032264a2ee9e990d94916
arjun292619 <j.c.arjunreddy@gmail.com>
2025-09-25 23:06:31 -0700

    add openapi documentation definition

--- section2/accounts/src/main/java/com/sophocles/accounts/AccountsApplication.java
+++ section2/accounts/src/main/java/com/sophocles/accounts/AccountsApplication.java
@@ -1,11 +1,26 @@
 package com.sophocles.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(title = "Accounts Microservice REST api Documentation",
+                description = "Accounts Microservice REST api Documentation",
+                version = "V1",
+                contact = @Contact(
+                        name = "Arjun",
+                        email = "accountsMs@email.com")
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Accounts Microservice REST api Documentation"
+        ))
 public class AccountsApplication {
 
     public static void main(String[] args) {
