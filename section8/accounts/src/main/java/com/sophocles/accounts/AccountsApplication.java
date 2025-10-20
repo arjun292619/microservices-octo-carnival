ad63a82859a042758a900b5974dace3c2a25658c
arjun292619 <j.c.arjunreddy@gmail.com>
2025-10-19 23:23:45 -0700

    add section 8 and set up Eureka server

---
+++ section8/accounts/src/main/java/com/sophocles/accounts/AccountsApplication.java
@@ -0,0 +1,33 @@
+package com.sophocles.accounts;
+
+import com.sophocles.accounts.dto.AccountsContactInfoDto;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = {AccountsContactInfoDto.class})
+@OpenAPIDefinition(
+        info = @Info(title = "Accounts Microservice REST api Documentation",
+                description = "Accounts Microservice REST api Documentation",
+                version = "V1",
+                contact = @Contact(
+                        name = "Arjun",
+                        email = "accountsMs@email.com")
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Accounts Microservice REST api Documentation"
+        ))
+public class AccountsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountsApplication.class, args);
+    }
+
+}
---
+++ section8/accounts/src/main/java/com/sophocles/accounts/mapper/CustomerMapper.java
@@ -0,0 +1,20 @@
+package com.sophocles.accounts.mapper;
+
+import com.sophocles.accounts.dto.CustomerDto;
+import com.sophocles.accounts.entity.Customer;
+
+public class CustomerMapper {
+    public static CustomerDto mapToCustomerDto(Customer customer, CustomerDto customerDto) {
+        customerDto.setName(customer.getName());
+        customerDto.setEmail(customer.getEmail());
+        customerDto.setMobileNumber(customer.getMobileNumber());
+        return customerDto;
+    }
+
+    public static Customer mapToCustomer(CustomerDto customerDto, Customer customer) {
+        customer.setName(customerDto.getName());
+        customer.setEmail(customerDto.getEmail());
+        customer.setMobileNumber(customerDto.getMobileNumber());
+        return customer;
+    }
+}
