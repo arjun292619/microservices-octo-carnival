a49f4e3ba1a46da5322c0b8ba43e5b56f24be290
arjun292619 <j.c.arjunreddy@gmail.com>
2025-10-24 11:41:26 -0700

    add custom path config bean to GatewayserverApplication

--- section9/gatewayserver/src/main/java/com/sophocles/gatewayserver/GatewayserverApplication.java
+++ section9/gatewayserver/src/main/java/com/sophocles/gatewayserver/GatewayserverApplication.java
@@ -2,12 +2,36 @@ package com.sophocles.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayserverApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayserverApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayserverApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator customRouteConfig(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p ->
+                        p.path("/sophocles/accounts/**")
+                                .filters(f ->
+                                        f.rewritePath("/sophocles/accounts/(?<segment>.*)", "/${segment}"))
+                                .uri("lb://ACCOUNTS"))
+                .route(p ->
+                        p.path("/sophocles/cards/**")
+                                .filters(f ->
+                                        f.rewritePath("/sophocles/cards/(?<segment>.*)", "/${segment}"))
+                                .uri("lb://CARDS"))
+                .route(p ->
+                        p.path("/sophocles/loans/**")
+                                .filters(f ->
+                                        f.rewritePath("/sophocles/loans/(?<segment>.*)", "/${segment}"))
+                                .uri("lb://LOANS"))
+                .build();
+    }
 
 }
