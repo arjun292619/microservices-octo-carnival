eaa91edbf33f881e9554b3f2fbcded9e8f7fa133
arjun292619 <j.c.arjunreddy@gmail.com>
2025-11-13 11:24:40 -0800

    add debug statements to application code

--- section11/accounts/src/main/java/com/sophocles/accounts/controller/CustomerController.java
+++ section11/accounts/src/main/java/com/sophocles/accounts/controller/CustomerController.java
@@ -52,8 +52,9 @@ public class CustomerController {
                                                                    @RequestParam
                                                                    @Pattern(regexp = "(^$|[0-9]{10})", message = "mobile number must be 10 digits")
                                                                    String mobileNumber) {
-        logger.debug("bank-correlation-id found: {}", correlationId);
+        logger.debug("fetchCustomerDetails method start");
         CustomerDetailsDto customerDetailsDto = iCustomerService.fetchCustomerDetails(mobileNumber, correlationId);
+        logger.debug("fetchCustomerDetails method end");
         return ResponseEntity.status(HttpStatus.OK).body(customerDetailsDto);
     }
 }
